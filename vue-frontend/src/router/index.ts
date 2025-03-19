2919b147340650e61c322166eeaa9e5ec088188f
Kyan mensen <91885904+IAm2XTr33M@users.noreply.github.com>
2025-03-19 11:02:57 +0100

    Added vue shims
    and some minor changes

--- vue-frontend/src/router/index.ts
+++ vue-frontend/src/router/index.ts
@@ -4,48 +4,50 @@ import DashboardView from '../views/DashboardView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import HomeView from '../views/HomeView.vue';
 
-async function isUserAuthenticated() {
+const securedRoutes = ["dashboard", "profile"];
 
-    try {
-      const response = await fetch('http://localhost:5001/validate', {
-        credentials: 'include',
-      });
-      return response.ok;
-    } catch (error) {
-      return false;
-    }
+async function isUserAuthenticated() {
+  try {
+    //Change this url to the url of your running hanko backend
+    const response = await fetch('http://localhost:5001/validate', {
+      credentials: 'include',
+    });
+    return response.ok;
+  } catch (error) {
+    return false;
   }
-  
+}
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-      {
-        path: '/',
-        name: 'home',
-        component: HomeView
-      },
-      {
-        path: '/dashboard',
-        name: 'dashboard',
-       component: DashboardView
-      },
-      {
-        path: '/profile',
-        name: 'profile',
-       component: ProfileView
-      },
-    ],
-  })
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+    component: DashboardView
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+    component: ProfileView
+    },
+  ],
+})
+
+//Middleware that runs before each navigation
+router.beforeEach(async (to, from, next) => {
+  //Check if you are on a secure route
+  if (typeof to.name === "string" && securedRoutes.includes(to.name) && !(await isUserAuthenticated())) {
+    next({ name: 'home' }); //Name of the route to redirect to if user is not authenticated
+  } else {
+    next();
+  }
+});
   
-  
-  router.beforeEach(async (to, from, next) => {
-    if ((to.name === 'dashboard' || to.name === 'profile') && !(await isUserAuthenticated())) {
-      next({ name: 'home' });
-    } else {
-      next();
-    }
-  });
-  
-  export default router
+export default router
   
--- vue-frontend/src/views/HomeView.vue
+++ vue-frontend/src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
   import HankoAuth from '../components/hanko/HankoAuth.vue'
   import HankoStarterInfo from '../components/starter/HankoStarterInfo.vue'
-
 </script>
 
 <template>
